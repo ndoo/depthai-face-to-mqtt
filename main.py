b1a03ffaf7b1c315f65583c2cdb25c2e189dd8d0
Andrew Yong <me@ndoo.sg>
2021-08-17 20:47:41 +0800

    Refactor code; add auto-enroll; use Python logging; Throttle recognition callback
    Refactor code:
    * Refactor code and change indentation
    * Face recognition flow changed to enable auto-enroll
    
    Add auto-enroll:
    * Auto-enroll faces below 20% confidence as a new face
    * Do not read databases again when enrolling, use database in memory
    * Limit database to 200 datasets each
    * Save a reference image with the first enroll
    * Added `-n`|`--no-enroll` argument to disable this feature
    
    Use Python logging
    * Default level id INFO
    * Added `-v`|`--verbose` argument to use log level DEBUG
    
    Throttle recognition callback
    * Used throttle library to wrap callback
    * Added `-t`|`--throttle` argument to set interval between callbacks

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from queue import Queue
 import argparse
 from time import monotonic
+import datetime
 import throttle
+import logging
 
 import cv2
 import depthai
@@ -12,41 +14,39 @@ import numpy as np
 from imutils.video import FPS
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-d", "--debug", action="store_true", help="Debug output"
-)
 
-parser.add_argument(
-    "-e",
-    "--enroll",
-    type=str,
-    default="",
-    help="Name to enroll",
-)
-
-parser.add_argument(
-    "-t",
-    "--throttle",
-    type=int,
-    default=10,
-    help="Seconds to throttle recognition alerts",
-)
+parser.add_argument("-p", "--preview", action="store_true",
+                    help="Preview camera")
+parser.add_argument("-b", "--database-dir", type=str, default="databases",
+                    help="Path to save/load recognition databases")
+parser.add_argument("-n", "--no-enroll", action="store_true",
+                    help="Do not auto-enroll")
+parser.add_argument("-t", "--throttle", type=int, default=10,
+                    help="Seconds to throttle recognition alerts")
+parser.add_argument("-v", "--verbose", help="Increase output verbosity",
+                    action="store_true")
 
 args = parser.parse_args()
 
-debug = args.debug
+preview       = args.preview
+noenroll      = args.noenroll
+throttle_secs = args.throttle
+db_dir        = args.database_dir
 
-@throttle.wrap(args.throttle, 1)
+log_level = logging.DEBUG if args.verbose else logging.INFO
+logging.basicConfig(level=log_level,
+    format='%(asctime)s - %(levelname)s - %(message)s')
+
+@throttle.wrap(throttle_secs, 1)
 def face_detected(name):
+    logging.info(f"Face detected was called")
     print(f"{name}\a")
 
 def to_planar(arr: np.ndarray, shape: tuple):
     return cv2.resize(arr, shape).transpose((2, 0, 1)).flatten()
 
-
 def to_nn_result(nn_data):
     return np.array(nn_data.getFirstLayerFp16())
-
 
 def run_nn(x_in, x_out, in_dict):
     nn_data = depthai.NNData()
@@ -55,12 +55,10 @@ def run_nn(x_in, x_out, in_dict):
     x_in.send(nn_data)
     return x_out.tryGet()
 
-
 def frame_norm(frame, *xy_vals):
     return (
         np.clip(np.array(xy_vals), 0, 1) * np.array(frame * (len(xy_vals) // 2))[::-1]
     ).astype(int)
-
 
 def correction(frame, angle=None, invert=False):
     h, w = frame.shape[:2]
@@ -78,47 +76,44 @@ def correction(frame, angle=None, invert=False):
         return corr, affine
     return corr
 
-
 def cosine_distance(a, b):
     if a.shape != b.shape:
         raise RuntimeError("array {} shape not match {}".format(a.shape, b.shape))
     a_norm = np.linalg.norm(a)
     b_norm = np.linalg.norm(b)
     similarity = np.dot(a, b.T) / (a_norm * b_norm)
-
     return similarity
 
+def enroll(name, face_frame, results, labels, db_dic):
 
-databases = "databases"
+    if name in db_dic:
+        db_ = db_dic[name]
+        if len(db_) >= 200:
+            logging.info(f"Not enrolling {name}, too many records.")
+            return
+        db_.append(np.array(results))
+    else:
+        labels.add(name)
+        db_ = np.array(results)
+        db_dic[name] = db_
+        # Save an image if newly enrolling
+        logging.info(f"Enrolling new face as {name}")
+        cc = face_frame.copy()
+        cv2.imwrite(f"{db_dir}/{name}.jpg", cc)
 
-if not os.path.exists(databases):
-    os.mkdir(databases)
-
-
-def create_db(face_frame, results):
-    name = args.enroll
-    font = cv2.FONT_HERSHEY_PLAIN
-    font_scale = 1
-    font_color = (0, 255, 0)
-    line_type = 1
-    try:
-        with np.load(f"{databases}/{name}.npz") as db:
-            db_ = [db[j] for j in db.files][:]
-    except Exception as e:
-        db_ = []
-    db_.append(np.array(results))
-    np.savez_compressed(f"{databases}/{name}", *db_)
-
+    if not os.path.exists(db_dir):
+        os.mkdir(db_dir)
+    np.savez_compressed(f"{db_dir}/{name}", *db_)
 
 def read_db(labels):
-    for file in os.listdir(databases):
+    for file in os.listdir(db_dir):
         filename = os.path.splitext(file)
         if filename[1] == ".npz":
             label = filename[0]
             labels.add(label)
     db_dic = {}
     for label in list(labels):
-        with np.load(f"{databases}/{label}.npz") as db:
+        with np.load(f"{db_dir}/{label}.npz") as db:
             db_dic[label] = [db[j] for j in db.files]
     return db_dic
 
@@ -127,7 +122,7 @@ class DepthAI:
     def __init__(
         self
     ):
-        print("Loading pipeline...")
+        logging.debug("Loading pipeline...")
         self.fps_cam = FPS()
         self.fps_nn = FPS()
         self.create_pipeline()
@@ -136,15 +131,16 @@ class DepthAI:
         self.lineType = 0
 
     def create_pipeline(self):
-        print("Creating pipeline...")
+        logging.debug("Creating pipeline...")
         self.pipeline = depthai.Pipeline()
 
         # ColorCamera
-        print("Creating Color Camera...")
+        logging.debug("Creating Color Camera...")
         self.cam = self.pipeline.createColorCamera()
         self.cam.setPreviewSize(self._cam_size[1], self._cam_size[0])
+        self.cam.setPreviewSize(self._cam_size[1], self._cam_size[0])
         self.cam.setResolution(
-            depthai.ColorCameraProperties.SensorResolution.THE_1080_P
+            depthai.ColorCameraProperties.SensorResolution.THE_4_K
         )
         self.cam.setInterleaved(False)
         self.cam.setBoardSocket(depthai.CameraBoardSocket.RGB)
@@ -156,7 +152,7 @@ class DepthAI:
 
         self.create_nns()
 
-        print("Pipeline created.")
+        logging.info("Pipeline created.")
 
     def create_nns(self):
         pass
@@ -170,12 +166,12 @@ class DepthAI:
         :return:
         """
         # NeuralNetwork
-        print(f"Creating {model_path} Neural Network...")
+        logging.debug(f"Creating {model_path} Neural Network...")
         model_nn = self.pipeline.createNeuralNetwork()
         model_nn.setBlobPath(str(Path(f"{model_path}").resolve().absolute()))
         model_nn.input.setBlocking(False)
         if first:
-            print("linked cam.preview to model_nn.input")
+            logging.debug("linked cam.preview to model_nn.input")
             self.cam.preview.link(model_nn.input)
         else:
             model_in = self.pipeline.createXLinkIn()
@@ -202,7 +198,7 @@ class DepthAI:
         :return:
         """
         # NeuralNetwork
-        print(f"Creating {model_path} Neural Network...")
+        logging.debug(f"Creating {model_path} MobileNet Neural Network...")
         model_nn = self.pipeline.createMobileNetDetectionNetwork()
         model_nn.setBlobPath(str(Path(f"{model_path}").resolve().absolute()))
         model_nn.setConfidenceThreshold(conf)
@@ -220,8 +216,12 @@ class DepthAI:
         model_nn.out.link(model_nn_xout.input)
 
     def start_pipeline(self):
-        self.device = depthai.Device(self.pipeline)
-        print("Starting pipeline...")
+        try:
+            logging.info("Starting pipeline...")
+            self.device = depthai.Device(self.pipeline)
+        except Exception as e:
+            logging.critical("Could not create pipeline: %s", str(e))
+            exit(1)
 
         self.start_nns()
 
@@ -232,7 +232,8 @@ class DepthAI:
     def start_nns(self):
         pass
 
-    def put_text(self, text, dot, color=(0, 0, 255), font_scale=None, line_type=None):
+    def put_text(self, text, dot, color=(0, 0, 255), font_scale=None,
+        line_type=None):
         font_scale = font_scale if font_scale else self.fontScale
         line_type = line_type if line_type else self.lineType
         dot = tuple(dot[:2])
@@ -256,13 +257,13 @@ class DepthAI:
         )
 
     def parse(self):
-        if debug:
+        if preview:
             self.debug_frame = self.frame.copy()
 
         s = self.parse_fun()
         # if s :
         #     raise StopIteration()
-        if debug:
+        if preview:
             cv2.imshow(
                 "Camera_view",
                 self.debug_frame,
@@ -272,7 +273,7 @@ class DepthAI:
                 cv2.destroyAllWindows()
                 self.fps_cam.stop()
                 self.fps_nn.stop()
-                print(
+                logging.debug(
                     f"FPS_CAMERA: {self.fps_cam.fps():.2f} , FPS_NN: {self.fps_nn.fps():.2f}"
                 )
                 raise StopIteration()
@@ -314,20 +315,15 @@ class Main(DepthAI):
         self.face_frame = Queue()
         self.face_coords = Queue()
         self.labels = set()
-        if len(args.enroll) == 0:
-            self.db_dic = read_db(self.labels)
+        self.db_dic = read_db(self.labels)
 
     def create_nns(self):
 
-        # Require higher confidence for enroll to prevent capturing low-confidence face matches
-        mobilenet_conf = 0.5 if len(args.enroll) == 0 else 1.0
-
         self.create_mobilenet_nn(
-            #"models/face-detection-retail-0004_openvino_2021.2_4shave.blob",
             "models/face-detection-retail-0005_openvino_2021.4_4shave.blob",
             "mfd",
             first=True,
-            conf=mobilenet_conf,
+            conf=0.9, # Raised to prevent auto-enroll of non-faces
         )
 
         self.create_nn(
@@ -360,7 +356,7 @@ class Main(DepthAI):
                 self.frame[face_coord[1] : face_coord[3], face_coord[0] : face_coord[2]]
             )
             self.face_coords.put(face_coord)
-            if debug:
+            if preview:
                 self.draw_bbox(face_coord, (10, 245, 10))
 
         return True
@@ -397,36 +393,44 @@ class Main(DepthAI):
             self.fps_nn.update()
             results = to_nn_result(nn_data)
 
-            if len(args.enroll) > 0:
-                create_db(face_frame, results)
-            else:
-                conf = []
-                max_ = 0
-                label_ = None
-                for label in list(self.labels):
-                    for j in self.db_dic.get(label):
-                        conf_ = cosine_distance(j, results)
-                        if conf_ > max_:
-                            max_ = conf_
-                            label_ = label
-                conf.append((max_, label_))
-                # name = conf[0] if conf[0][0] >= 0.5 else (1 - conf[0][0], "UNKNOWN")
-                if conf[0][0] >= 0.6:
-                    name = conf[0]
-                    face_detected(name[1])
-                else:
-                    name = (1 - conf[0][0], "UNKNOWN")
-                if debug:
-                    self.put_text(
-                        f"name:{name[1]}",
-                        (face_coords[0], face_coords[1] - 35),
-                        (244, 0, 255),
-                    )
-                    self.put_text(
-                        f"conf:{name[0] * 100:.2f}%",
-                        (face_coords[0], face_coords[1] - 10),
-                        (244, 0, 255),
-                    )
+            conf = []
+            max_ = 0
+            label_ = None
+            for label in list(self.labels):
+                for j in self.db_dic.get(label):
+                    conf_ = cosine_distance(j, results)
+                    if conf_ > max_:
+                        max_ = conf_
+                        label_ = label
+            conf.append((max_, label_))
+            
+            name = conf[0]
+
+            if name[0] >= 0.7:
+                # Use debug log level to minimize screen scroll
+                logging.debug(f"Face detected: {name[1]}; confidence: {name[0] * 100:.2f}%\a")
+                face_detected(name[1])
+            elif name[0] > 0.6:
+                logging.info(f"Face detected, updating enrolment: {name[1]}; confidence: {name[0] * 100:.2f}%\a")
+                enroll(name[1], face_frame, results, self.labels,
+                       self.db_dic)
+            elif name[0] < 0.2:
+                logging.info(f"Face detected, enrolling: {name[1]}; confidence: {name[0] * 100:.2f}%\a")
+                enroll(datetime.datetime.now().isoformat().replace(':','_'),
+                    face_frame, results, self.labels, self.db_dic)
+
+            if preview:
+                self.put_text(
+                    f"name:{name[1]}",
+                    (face_coords[0], face_coords[1] - 35),
+                    (244, 0, 255),
+                )
+                self.put_text(
+                    f"conf:{name[0] * 100:.2f}%",
+                    (face_coords[0], face_coords[1] - 10),
+                    (244, 0, 255),
+                )
+
         return True
 
     def parse_fun(self):
